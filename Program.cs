be8c1fc161600550a031428ccb74de1fc3ff7f04
Mars <mallorysmith6464@gmail.com>
2019-09-17 12:06:13 -0400

    did code kata

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace digits_explosion_kata
+{
+
+  public class Digits
+  {
+    public static string Explode(string s)
+    {
+      //not doing it on index doing it on value so use one parameter on select
+      var x = String.Join("",
+      s.Select(
+          (letter, i) =>
+          String.Join("", Enumerable.Repeat(letter, (int)Char.GetNumericValue(letter)))));
+
+      return x;
+    }
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
